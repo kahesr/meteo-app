f4f48575b07cb632a786ed7d2b231541a9083d3f
kahesr <60002689+kahesr@users.noreply.github.com>
2024-12-21 15:34:56 +0330

    Added live weather data using API

--- src/script.js
+++ src/script.js
@@ -1,9 +1,19 @@
 function handleApi(response) {
-  console.log(response.data);
-  document.querySelector("#city").innerHTML = response.data.city;
-  document.querySelector("#current-temp").innerHTML = Math.round(
-    response.data.temperature.current
-  );
+  let city = response.data.city;
+  let temperature = response.data.temperature.current;
+  let humidity = response.data.temperature.humidity;
+  let wind = response.data.wind.speed;
+  let weatherCondition = response.data.condition.description;
+  let iconImage = `<img src=${response.data.condition.icon_url} alt="">`;
+  let date = new Date(response.data.time * 1000);
+
+  document.querySelector("#city").innerHTML = city;
+  document.querySelector("#current-temp").innerHTML = Math.round(temperature);
+  document.querySelector("#humidity").innerHTML = `${humidity}%`;
+  document.querySelector("#wind").innerHTML = `${wind}km/h`;
+  document.querySelector("#weather-description").innerHTML = weatherCondition;
+  document.querySelector("#icon").innerHTML = iconImage;
+  document.querySelector("#time").innerHTML = formatDate(date);
 }
 
 function searchCity(city) {
@@ -18,6 +28,32 @@ function handleSearchSubmit(event) {
   searchCity(formInput.value);
 }
 
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+
+  let minute = date.getMinutes();
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+
+  return `${day} ${hour}:${minute}`;
+}
+
+//////////////////////////////////////////
 let form = document.querySelector("#form");
 form.addEventListener("submit", handleSearchSubmit);
 
