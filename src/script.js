eb47a9347f6889d26ffc83a8461ce79c1abda11f
kahesr <60002689+kahesr@users.noreply.github.com>
2024-12-22 15:46:16 +0330

    Weather forecast - API Integration

--- src/script.js
+++ src/script.js
@@ -1,10 +1,10 @@
-function handleApi(response) {
+function displayCurrentTemp(response) {
   let city = response.data.city;
   let temperature = response.data.temperature.current;
   let humidity = response.data.temperature.humidity;
   let wind = response.data.wind.speed;
   let weatherCondition = response.data.condition.description;
-  let iconImage = `<img src=${response.data.condition.icon_url} alt="">`;
+  let iconImage = `<img src=${response.data.condition.icon_url} alt="" class="icon">`;
   let date = new Date(response.data.time * 1000);
 
   document.querySelector("#city").innerHTML = city;
@@ -14,37 +14,34 @@ function handleApi(response) {
   document.querySelector("#weather-description").innerHTML = weatherCondition;
   document.querySelector("#icon").innerHTML = iconImage;
   document.querySelector("#time").innerHTML = formatDate(date);
+
+  getForcast(response.data.city);
 }
 
-function displayForcast() {
+function displayForcast(response) {
+  let array = ["1", "2", "3", "4", "5"]; //array of day indexes
   let forcast = document.querySelector("#forcast");
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu"];
+  let dailyForcast = response.data.daily; //array of 7 days
+  forcast.innerHTML = "";
 
-  days.forEach(function (day) {
+  array.forEach(function (dayIndex) {
+    let highTemp = Math.round(dailyForcast[dayIndex].temperature.maximum);
+    let lowTemp = Math.round(dailyForcast[dayIndex].temperature.minimum);
+    let iconUrl = dailyForcast[dayIndex].condition.icon_url;
+    let icon = `<img src=${iconUrl} alt="" class="icon">`;
+    let date = new Date(dailyForcast[dayIndex].time * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let day = days[date.getDay()];
+
     forcast.innerHTML += `<div class="day-forcast-container">
     <div class="day forcastDetail">${day}</div>
-    <div class="icon forcastDetail">🌦️</div>
+    <div class="forcastDetail">${icon}</div>
     <div class="temps forcastDetail">
-    <span class="temp highestTemp">15°</span>
-    <span class="temp loestTemp">9°</span>
+    <span class="temp highestTemp">${highTemp}°</span>
+    <span class="temp loestTemp">${lowTemp}°</span>
     </div>
     </div>`;
   });
-}
-
-function searchCity(city) {
-  let apiKey = "taf74bb8e045996263fo9880fc8a042e";
-  let api = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(api).then(handleApi);
-
-  //let forcastApi = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
-  //axios.get(forcastApi).then(handleForcastApi);
-}
-
-function handleSearchSubmit(event) {
-  event.preventDefault();
-  let formInput = document.querySelector("#form-input");
-  searchCity(formInput.value);
 }
 
 function formatDate(date) {
@@ -72,10 +69,26 @@ function formatDate(date) {
   return `${day} ${hour}:${minute}`;
 }
 
+function getForcast(city) {
+  let apiKey = "taf74bb8e045996263fo9880fc8a042e";
+  let forcastApi = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(forcastApi).then(displayForcast);
+}
+
+function searchCity(city) {
+  let apiKey = "taf74bb8e045996263fo9880fc8a042e";
+  let api = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(api).then(displayCurrentTemp);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let formInput = document.querySelector("#form-input");
+  searchCity(formInput.value);
+}
+
 //////////////////////////////////////////
 let form = document.querySelector("#form");
 form.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
-
-displayForcast();
